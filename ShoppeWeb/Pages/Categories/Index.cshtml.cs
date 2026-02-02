8550000b50f97e7fd5fa6d75b7cda7ee56cf440b
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-02-02 22:02:09 +0530

    Display all categories

---
+++ ShoppeWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShoppeWeb.Data;
+using ShoppeWeb.Models;
+
+namespace ShoppeWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            // Retrive all categories from the database
+            Categories = _db.Category;
+        }
+    }
+}
