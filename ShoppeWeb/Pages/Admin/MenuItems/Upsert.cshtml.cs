a1b2f36dfb61db1f19986af3fa674a01ea6da825
sandeep <deepbluechemical@gmail.com>
2026-02-04 14:34:35 +0530

    UPSERT contd, Delete old image, Tracking Issue

--- ShoppeWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ ShoppeWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Shoppe.DataAccess.Repository;
 using Shoppe.Models;
 using ShoppeWeb.DataAccess.Data;
 using ShoppeWeb.Models;
+using System;
 
 
 namespace ShoppeWeb.Pages.Admin.MenuItems
@@ -29,8 +31,13 @@ namespace ShoppeWeb.Pages.Admin.MenuItems
             _hostEnvirnment = env;
             MenuItem = new();
         }
-        public void OnGet()
+        public void OnGet(int? id)
         {
+            if (id != null)
+            {
+                // get the items
+                MenuItem = _db.MenuItem.FirstOrDefault(i=>i.Id == id);
+            }
             CategoryList = _db.Category.Select(i=> new SelectListItem(){
                 Text = i.Name,
                 Value = i.id.ToString(),
@@ -45,6 +52,7 @@ namespace ShoppeWeb.Pages.Admin.MenuItems
 
         public async Task<IActionResult> OnPost()
         {
+           
             string webRootPath = _hostEnvirnment.WebRootPath;
             var files = HttpContext.Request.Form.Files;
             if (MenuItem.Id == 0)
@@ -63,8 +71,37 @@ namespace ShoppeWeb.Pages.Admin.MenuItems
                 await _db.SaveChangesAsync();
             }else{
                 // Edit
-            }
+                // get the items
+                var objFromDb = await _db.MenuItem.AsNoTracking().FirstOrDefaultAsync(i => i.Id == MenuItem.Id);
+                // check file is uplaoded again or not
+                if (files.Count > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\menu_items");
+                    var extension = Path.GetExtension(files[0].FileName);
 
+                    // Delete old file
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.Image.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                    // New uplaod
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    MenuItem.Image = @"\images\menu_items\" + fileName + extension;
+                }
+                else
+                {
+                    MenuItem.Image = objFromDb.Image;
+                }
+
+                // Add to database
+                _db.MenuItem.Update(MenuItem);
+                await _db.SaveChangesAsync();
+            }
             return RedirectToPage("Index");
         }
     }
