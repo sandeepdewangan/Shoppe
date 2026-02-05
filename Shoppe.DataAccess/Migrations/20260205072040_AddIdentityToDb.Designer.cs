25b667588f49025dbf09d140ef29f00541d9fa3f
sandeep <deepbluechemical@gmail.com>
2026-02-05 12:51:35 +0530

    Identity DB Migration

--- Shoppe.DataAccess/Migrations/20260205072040_AddIdentityToDb.Designer.cs
+++ Shoppe.DataAccess/Migrations/20260205072040_AddIdentityToDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShoppeWeb.DataAccess.Data;
@@ -11,9 +12,11 @@ using ShoppeWeb.DataAccess.Data;
 namespace Shoppe.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205072040_AddIdentityToDb")]
+    partial class AddIdentityToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
