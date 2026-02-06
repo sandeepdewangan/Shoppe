4669d6f2358803e2fe74afdd6f19ff6f70a25b20
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-02-06 22:12:05 +0530

    Custom Handler -> on button press

--- ShoppeWeb/Pages/Customer/Cart.cshtml.cs
+++ ShoppeWeb/Pages/Customer/Cart.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Shoppe.Models;
@@ -29,12 +30,41 @@ namespace ShoppeWeb.Pages.Customer
                 ShoppingCartList = _db.ShoppingCart.Include(m => m.MenuItem).ThenInclude(c => c.Category)
                     .Include(m => m.MenuItem).ThenInclude(f => f.FoodType).Where(c => c.ApplicationUserId == claim.Value).ToList();
 
+
                 // calculate cart total
                 foreach (var cart in ShoppingCartList)
                 {
                     CartTotal = CartTotal + (cart.MenuItem.Price * cart.Count);
                 }
             }
+        }
+
+
+        public IActionResult OnPostIncrement(int cartId)
+        {
+            var cart = _db.ShoppingCart.FirstOrDefault(u => u.Id == cartId);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            cart.Count = cart.Count + 1;
+            _db.ShoppingCart.Update(cart);
+            _db.SaveChanges();
+
+            return RedirectToPage("/Customer/Cart");
+        }
+
+        public async Task<IActionResult> OnPostRemove(int cartId)
+        {
+            var item = await _db.ShoppingCart.FindAsync(cartId);
+
+            if (item != null)
+            {
+                _db.ShoppingCart.Remove(item);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage("/Customer/Cart");
         }
     }
 }
